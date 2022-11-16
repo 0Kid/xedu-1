235bae4737f49d84f50a8f68469865331244b074
hanifann <hanifannurulhaq@outlook.com>
2022-11-16 21:19:53 +0700

    implement data source for news

--- lib/features/home/data/datasources/news_local_datasource.dart
+++ lib/features/home/data/datasources/news_local_datasource.dart
@@ -1,6 +1,33 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:xedu/core/error/exception.dart';
 import 'package:xedu/features/home/data/models/news_model.dart';
+
+const CACHED_NEWS = 'CACHED_NEWS';
 
 abstract class NewsLocalDatasource {
   Future<NewsModel>? getLastNews();
   Future<void>? cachedNews(NewsModel mews);
+}
+
+class NewsLocalDatasourceImpl extends NewsLocalDatasource {
+  final SharedPreferences prefs;
+
+  NewsLocalDatasourceImpl({required this.prefs});
+
+  @override
+  Future<void>? cachedNews(NewsModel mews) {
+    return null;
+  }
+
+  @override
+  Future<NewsModel>? getLastNews() {
+    final lastNews = prefs.getString(CACHED_NEWS);
+    if(lastNews != null) {
+      return Future.value(NewsModel.fromJson(jsonDecode(lastNews)));
+    } else {
+      throw CacheException();
+    }
+  }  
 }
--- lib/features/home/data/datasources/news_remote_datasource.dart
+++ lib/features/home/data/datasources/news_remote_datasource.dart
@@ -1,5 +1,26 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:xedu/core/error/exception.dart';
 import 'package:xedu/features/home/data/models/news_model.dart';
+import 'package:xedu/utils/constant.dart';
 
 abstract class NewsRemoteDataSource {
   Future<NewsModel>? getRemoteNews();
+}
+
+class NewsRemoteDataSourceImpl implements NewsRemoteDataSource {
+  final Client client;
+
+  NewsRemoteDataSourceImpl({required this.client});
+  @override
+  Future<NewsModel>? getRemoteNews() async {
+    final response = await client.get(Uri.http(URL, '/api/berita'));
+    if (response.statusCode == 200) {
+      return NewsModel.fromJson(jsonDecode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
+  
 }
