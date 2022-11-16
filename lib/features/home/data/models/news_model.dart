917d185834509193cad5643fe4f76e61dc16a703
hanifann <hanifannurulhaq@outlook.com>
2022-11-16 20:45:57 +0700

    add model for news

---
+++ lib/features/home/data/models/news_model.dart
@@ -0,0 +1,29 @@
+import 'package:xedu/features/home/domain/entity/news.dart';
+
+class NewsModel extends News {
+  NewsModel({required super.data});
+  
+  factory NewsModel.fromJson(Map<String, dynamic> json) {
+    return NewsModel(
+      data: List<NewsDataModel>.from(json["data"].map((x)=>NewsDataModel.fromJson(x)))
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    "data": List<dynamic>.from(data.map((e) => e.toJson()))
+  };
+}
+
+class NewsDataModel extends NewsData {
+  NewsDataModel({required super.id, required super.judul, required super.image, required super.content});
+  
+  factory NewsDataModel.fromJson(Map<String, dynamic> json) {
+    return NewsDataModel(
+      id: json['id'], 
+      judul: json['judul'], 
+      image: json['image'], 
+      content: json['content']
+    );
+  }
+  
+}
---
+++ test/home/data/models/news_model_test.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:xedu/features/home/data/models/news_model.dart';
+import 'package:xedu/features/home/domain/entity/news.dart';
+
+import '../../../fixtures/fixture_reader.dart';
+
+void main() {
+  final tNewsDataModel = NewsDataModel(id: 1, judul: 'judul', image: 'image', content: 'content');
+  final tNewsData = [tNewsDataModel];
+  final tNewsModel = NewsModel(data: tNewsData);
+
+  test('should be subclass of News entiry', () async {
+    //expect
+    expect(tNewsDataModel, isA<NewsData>());
+    expect(tNewsModel, isA<News>());
+  });
+
+  group('fromJson', () {
+    test('should return valid json model', () async {
+      //arrange
+      Map<String, dynamic> jsonMap = jsonDecode(fixture('news.json'));
+      //act
+      final result = NewsModel.fromJson(jsonMap);
+      //assert
+      expect(result, equals(tNewsModel));
+    });
+  });
+
+  group('toJson', () {
+    test('should return json from model', () async {
+      //arange
+      final result = tNewsModel.toJson();
+      //act
+      final expectedMap = jsonDecode(fixture('news.json'));
+      //assert
+      expect(result, equals(expectedMap));
+    });
+  });
+}
