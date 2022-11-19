7d889280294605c7c94a58bd828b497e4fe4a1aa
hanifan <hanifannurulhaq@outlook.com>
2022-11-19 16:21:42 +0700

    fix parameter

--- lib/features/register/domain/repositories/register_repository.dart
+++ lib/features/register/domain/repositories/register_repository.dart
@@ -3,5 +3,14 @@ import 'package:xedu/core/error/failures.dart';
 import 'package:xedu/features/register/domain/entity/register.dart';
 
 abstract class RegisterRepository {
-  Future<Either<Failure, Register>>? postRegistration(Register register);
+  Future<Either<Failure, Register>>? postRegistration({
+    String email,
+    String namaLengkap,
+    int umur,
+    String alamat,
+    String noTelp,
+    int sekolahId,
+    String jenisKelamin,
+    String password
+  });
 }
