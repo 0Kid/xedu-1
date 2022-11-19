b1be7ddea58b45dfbf89c718af1346296972765c
hanifan <hanifannurulhaq@outlook.com>
2022-11-19 16:21:52 +0700

    impelemnt datasource

--- lib/features/register/data/datasource/register_remote_datasource.dart
+++ lib/features/register/data/datasource/register_remote_datasource.dart
@@ -1,5 +1,46 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:xedu/core/error/exception.dart';
 import 'package:xedu/features/register/data/model/register_model.dart';
+import 'package:xedu/utils/constant.dart';
 
 abstract class RegisterRemoteDatasource {
-  Future<RegisterModel>? remoteRegister(String email, String namaLengkap, int umur, String alamat, String noTelp, int sekolahId, String jenisKelamin, String password);
+  Future<RegisterModel>? remoteRegister({
+    required String email, 
+    required String namaLengkap, 
+    required int umur, 
+    required String alamat, 
+    required String noTelp, 
+    required int sekolahId, 
+    required String jenisKelamin, 
+    required String password
+  });
+}
+
+class RegisterRemoteDatasourceImpl extends RegisterRemoteDatasource {
+  final Client client;
+
+  RegisterRemoteDatasourceImpl({required this.client});
+  @override
+  Future<RegisterModel>? remoteRegister({required String email, required String namaLengkap, required int umur, required String alamat, required String noTelp, required int sekolahId, required String jenisKelamin, required String password}) async {
+    final response = await client.post(
+      Uri.http(URL, '/api/signup'),
+      body: {
+        "email": email,
+        "nama_lengkap": namaLengkap,
+        "umur": umur,
+        "alamat": alamat,
+        "notelp": noTelp,
+        "sekolahId": sekolahId,
+        "jenis_kelamin": jenisKelamin,
+        "password": password
+      }
+    );
+    if(response.statusCode == 200){
+      return RegisterModel.fromJson(jsonDecode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
 }
