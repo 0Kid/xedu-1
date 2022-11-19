31777cc74fa8cb168c8af4d42acc0003289c013d
hanifan <hanifannurulhaq@outlook.com>
2022-11-19 16:21:32 +0700

    implement register repository

--- lib/features/register/data/repositories/register_repository_impl.dart
+++ lib/features/register/data/repositories/register_repository_impl.dart
@@ -1,9 +1,10 @@
+import 'package:xedu/core/error/exception.dart';
 import 'package:xedu/core/platform/network_info.dart';
 import 'package:xedu/features/register/data/datasource/register_remote_datasource.dart';
 import 'package:xedu/core/error/failures.dart';
 import 'package:dartz/dartz.dart';
+import 'package:xedu/features/register/domain/entity/register.dart';
 import 'package:xedu/features/register/domain/repositories/register_repository.dart';
-import 'package:xedu/features/register/domain/usecase/register_usecase.dart';
 
 class RegisterRepositoryImpl extends RegisterRepository {
   final RegisterRemoteDatasource datasource;
@@ -13,11 +14,28 @@ class RegisterRepositoryImpl extends RegisterRepository {
     required this.datasource, 
     required this.networkInfo
   });
-
+  
   @override
-  Future<Either<Failure, void>>? postRegistration(RegisterParams register) async {
-    networkInfo.isConnected;
-    return Right(await datasource.remoteRegister(register)!);
+  Future<Either<Failure, Register>>? postRegistration({String? email, String? namaLengkap, int? umur, String? alamat, String? noTelp, int? sekolahId, String? jenisKelamin, String? password}) async {
+    if(await networkInfo.isConnected){
+      try {
+        final response = await datasource.remoteRegister(
+          email: email!,
+          namaLengkap: namaLengkap!,
+          umur: umur!,
+          alamat: alamat!,
+          noTelp: noTelp!,
+          sekolahId: sekolahId!,
+          jenisKelamin: jenisKelamin!,
+          password: password!
+        );
+        return Right(response!);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      return Left(ServerFailure());
+    }
   }
   
 }
