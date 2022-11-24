637c858226aa176bfe01b85176fb2743f34d931a
hanifann <hanifannurulhaq@outlook.com>
2022-11-24 10:09:42 +0700

    tambah parameter textAlign

--- lib/widgets/text_widget.dart
+++ lib/widgets/text_widget.dart
@@ -7,18 +7,21 @@ class CustomTextWidget extends StatelessWidget {
     this.size = 12, 
     this.weight = FontWeight.w400, 
     this.color = Colors.black,
+    this.textAlign = TextAlign.start
   }) : super(key: key);
 
   final String text;
   final double size;
   final FontWeight weight;
   final Color color;
+  final TextAlign textAlign;
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
       maxLines: 3,
+      textAlign: textAlign,
       style: TextStyle(
         fontSize: size,
         fontWeight: weight,
