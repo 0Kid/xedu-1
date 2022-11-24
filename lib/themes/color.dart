13282e26afe23989a1ae74e2349426433ff9bff5
hanifann <hanifannurulhaq@outlook.com>
2022-11-24 10:09:50 +0700

    tambah warna baru

--- lib/themes/color.dart
+++ lib/themes/color.dart
@@ -5,3 +5,4 @@ import 'package:flutter/material.dart';
 const Color kPrimaryColor = Color.fromRGBO(128, 69, 173, 1);
 const Color kPurpleTextColor = Color.fromRGBO(76, 40, 104, 1);
 const Color kAccenctColor = Color.fromRGBO(95, 0, 119, 1);
+const Color kGreenColor = Color.fromRGBO(75, 211, 123, 1);
